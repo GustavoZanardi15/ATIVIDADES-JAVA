9e59aea2a712d53aeb130755353a579b279c678a
Gustavo Ulian Zanardi <134564393+GustavoZanardi15@users.noreply.github.com>
2025-02-20 22:16:19 -0300

    Delete 3-LivrariaApplication directory

--- 3-LivrariaApplication/src/main/java/com/livraria/repository/LivroRepository.java
+++
@@ -1,7 +0,0 @@
-package com.livraria.repository;
-
-import com.livraria.model.Livro;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LivroRepository extends JpaRepository<Livro, Long> {
-}
