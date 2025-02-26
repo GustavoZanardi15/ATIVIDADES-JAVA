95f83476381a53865687124ce1f0ba81e954aa35
Gustavo Ulian Zanardi <134564393+GustavoZanardi15@users.noreply.github.com>
2025-02-26 15:32:42 -0300

    Delete gerenciamento-funcionarios/gerenciamento-funcionarios directory

--- gerenciamento-funcionarios/gerenciamento-funcionarios/src/main/java/com/example/gerenciamentofuncionarios/repository/FuncionarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.gerenciamentofuncionarios.repository;
-
-import com.example.gerenciamentofuncionarios.model.Funcionario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FuncionarioRepository extends JpaRepository<Funcionario, Integer> {
-}
