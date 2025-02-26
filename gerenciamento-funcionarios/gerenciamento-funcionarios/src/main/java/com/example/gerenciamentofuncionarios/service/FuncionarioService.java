383e399b99f47da75ee2537d84b566a18ec677b5
Gustavo Ulian Zanardi <134564393+GustavoZanardi15@users.noreply.github.com>
2025-02-26 15:32:24 -0300

    Add files via upload

---
+++ gerenciamento-funcionarios/gerenciamento-funcionarios/src/main/java/com/example/gerenciamentofuncionarios/service/FuncionarioService.java
@@ -0,0 +1,40 @@
+package com.example.gerenciamentofuncionarios.service;
+
+import com.example.gerenciamentofuncionarios.model.Funcionario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.gerenciamentofuncionarios.repository.FuncionarioRepository;
+
+import java.util.List;
+
+@Service
+public class FuncionarioService {
+
+    @Autowired
+    private FuncionarioRepository funcionarioRepository;
+
+    public List<Funcionario>listarTodos(){
+        return funcionarioRepository.findAll();
+    }
+
+    public Funcionario buscarFuncionarioPorId(int id){
+        return funcionarioRepository.findById(id).get();
+    }
+
+    public Funcionario salvar(Funcionario funcionario){
+        return funcionarioRepository.save(funcionario);
+    }
+
+    public Funcionario atualizarSalario(int id, double novoSalario) {
+        Funcionario funcionario = buscarFuncionarioPorId(id);
+        if (funcionario != null) {
+            funcionario.setSalario(novoSalario);
+            return funcionarioRepository.save(funcionario);
+        }
+        return null;
+    }
+
+    public void deletar(int id){
+        funcionarioRepository.deleteById(id);
+    }
+}
